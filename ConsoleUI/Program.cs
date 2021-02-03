57e7a36067e5cb82c4cdc36825e5c6d49af5899d
BURAK AYDEMIR <burak_aydemir86@hotmail.com>
2021-02-03 14:04:49 +0300

    03.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.InMemory;
-using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -10,17 +9,6 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new InMemoryCarDal());
-
-            foreach (var car in carManager.GetAll())
-            {
-                Console.WriteLine(car.Description);
-            }
-
-            carManager.Add(new Car {Id=6, BrandId=4, ColorId=7, DailyPrice=350000, ModelYear=2015, Description="Mercedes CLA" });
-            Car carSearch = carManager.GetById(6);
-            Console.WriteLine(carSearch.Description);
-
-            carManager.Delete(new Car { Id = 4, BrandId = 2, ColorId = 5, DailyPrice = 120000, ModelYear = 2017, Description = "Avensis" });
 
             foreach (var car in carManager.GetAll())
             {
